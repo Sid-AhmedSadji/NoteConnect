a47ed5e0e185cc490f41fab50ab40b8194d29e34
Sid-AhmedSadji <Sid-Ahmed_Sadji@hotmail.com>
2025-05-03 22:41:11 +0200

    first commit

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
-if (!process.env.REACT_APP_API_URL) 
+
+console.log(import.meta.env.VITE_API_URL);
+
+if (!import.meta.env.VITE_API_URL) 
     throw new Error('REACT_APP_API_URL is not defined in the environment variables');
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL, 
+    baseURL: import.meta.env.VITE_API_URL, 
     withCredentials: true, 
     headers: {
         'Content-Type': 'application/json',
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: Number(process.env.VITE_PORT) || 5173,
   },
   plugins: [
     react(),

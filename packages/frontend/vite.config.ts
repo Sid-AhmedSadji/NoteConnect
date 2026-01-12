0b6922be52664ec94bb197951157f3b000dd22ad
Sid-AhmedSadji <sid-ahmed_sadji@hotmail.com>
2026-01-12 01:44:40 +0100

    ssl

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -11,6 +12,10 @@ export default defineConfig(({ mode }) => ({
     host: "0.0.0.0",
     port: Number(process.env.VITE_PORT) || 5173,
     allowedHosts:process.env.VITE_ALLOWED_HOSTS ? process.env.VITE_ALLOWED_HOSTS.split(',') : [],
+    https:{
+      key: process.env.VITE_HTTPS_KEY_PATH ? fs.readFileSync(process.env.VITE_HTTPS_KEY_PATH) : undefined,
+      cert: process.env.VITE_HTTPS_CERT_PATH ? fs.readFileSync(process.env.VITE_HTTPS_CERT_PATH) : undefined,
+    }
   },
   preview: {
     port: Number(process.env.VITE_PORT) || 8080, // Preview mode should use 8080
